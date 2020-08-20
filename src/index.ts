62207a4fca09e731bbd91f006ad6db8d7d8f56db
lordie <levimanga@gmail.com>
2020-08-20 18:53:22 -0300

    stop @ users

--- src/index.ts
+++ src/index.ts
@@ -17,29 +17,31 @@ const stream = t.stream("statuses/filter", {
 
 stream.on("tweet", async (tweet: any) => {
   if (tweet.user.id_str == giraId) {
-    let mexicano = await axios({
-      method: "POST",
-      url: "https://api.cognitive.microsofttranslator.com/translate",
-      params: {
-        "api-version": "3.0",
-        to: ["es"],
-      },
-      headers: {
-        "Ocp-Apim-Subscription-Key": "e4a35a78334048e99820733b7aa62bd8",
-        "Content-type": "application/json",
-        "X-ClientTraceId": v4().toString(),
-        "Ocp-Apim-Subscription-Region": "canadacentral",
-      },
-      data: [
-        {
-          text: tweet.text,
+    if (tweet.text != "") {
+      let mexicano = await axios({
+        method: "POST",
+        url: "https://api.cognitive.microsofttranslator.com/translate",
+        params: {
+          "api-version": "3.0",
+          to: ["es"],
         },
-      ],
-    });
+        headers: {
+          "Ocp-Apim-Subscription-Key": "e4a35a78334048e99820733b7aa62bd8",
+          "Content-type": "application/json",
+          "X-ClientTraceId": v4().toString(),
+          "Ocp-Apim-Subscription-Region": "canadacentral",
+        },
+        data: [
+          {
+            text: tweet.text.replace(/@\w*/g, ""),
+          },
+        ],
+      });
 
-    t.post("statuses/update", {
-      status: mexicano.data[0].translations[0].text,
-      attachment_url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-    });
+      t.post("statuses/update", {
+        status: mexicano.data[0].translations[0].text,
+        attachment_url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+      });
+    }
   }
 });
