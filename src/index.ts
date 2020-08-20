139b50f291b34fe809ec82e09bbf905c8303b0ea
lordie <levimanga@gmail.com>
2020-08-19 21:28:23 -0300

    giralmex ready

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import twit from "twit";
+import axios from "axios";
+import { v4 } from "uuid";
+import secrets from "../secrets.json";
+
+const t: any = new twit({
+  ...secrets,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
+});
+
+const giraId = "983327756640489473";
+
+const stream = t.stream("statuses/filter", {
+  follow: giraId,
+});
+
+stream.on("tweet", async (tweet: any) => {
+  if (tweet.user.id_str == giraId) {
+    let mexicano = await axios({
+      method: "POST",
+      url: "https://api.cognitive.microsofttranslator.com/translate",
+      params: {
+        "api-version": "3.0",
+        to: ["es"],
+      },
+      headers: {
+        "Ocp-Apim-Subscription-Key": "e4a35a78334048e99820733b7aa62bd8",
+        "Content-type": "application/json",
+        "X-ClientTraceId": v4().toString(),
+        "Ocp-Apim-Subscription-Region": "canadacentral",
+      },
+      data: [
+        {
+          text: tweet.text,
+        },
+      ],
+    });
+
+    t.post("statuses/update", {
+      status: mexicano.data[0].translations[0].text,
+      attachment_url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+    });
+  }
+});
