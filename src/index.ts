a26c321bb233bef259653f7b3a16023df4366d58
lordie <levimanga@gmail.com>
2020-09-01 14:32:31 -0300

    added fancams, changed variable names

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,20 @@ import secrets from "../secrets.json";
 
 console.log("starting");
 
-const t: any = new twit({
-  ...secrets,
+const gira: any = new twit({
+  ...secrets.bot,
+  ...secrets.gira,
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true, // optional - requires SSL certificates to be valid.
 });
 
 const giraId = "983327756640489473";
 
-const stream = t.stream("statuses/filter", {
+const streamGira = gira.stream("statuses/filter", {
   follow: giraId,
 });
 
-console.log("working");
-
-stream.on("tweet", async (tweet: any) => {
+streamGira.on("tweet", async (tweet: any) => {
   if (tweet.user.id_str == giraId) {
     if (tweet.text != "") {
       console.log(tweet);
@@ -45,10 +44,33 @@ stream.on("tweet", async (tweet: any) => {
         ],
       });
 
-      t.post("statuses/update", {
+      gira.post("statuses/update", {
         status: mexicano.data[0].translations[0].text.replace(/newline/g, "\n"),
         attachment_url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
       });
     }
   }
 });
+
+////////////////////////KPOP
+
+const livia: any = new twit({
+  ...secrets.bot,
+  ...secrets.livia,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
+});
+
+const streamLivia = livia.stream("statuses/filter", {
+  track: ["@minatozaki_lily"],
+});
+
+streamLivia.on("tweet", ({ text }: { text: string }) => {
+  const fancam = text.match(/https:\/\/t.co\/\w*/g);
+  livia.post("statuses/update", {
+    status: fancam,
+    attachment_url: fancam,
+  });
+});
+
+console.log("working");
